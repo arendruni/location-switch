f53e5eaf23b38588f2b1a00dcd4fd98a33accb95
Andrea Giunio Bruni <dev@giun.io>
2023-03-26 11:20:55 +0200

    refactor: rename variables, add logs

--- locationSwitch.sh
+++ locationSwitch.sh
@@ -1,70 +1,62 @@
 #!/bin/sh
 
-# A Simple script run by a deamon every 10 seconds,
-# used to switch from one network location to another,
-# each location has specific parameters and is based
-# on different interfaces.
+# A Simple script used to switch from one network location to another, each location
+# has specific parameters and is based on different interfaces.
 #
-# Basically it checks if an ethernet interface is
-# connected then checks if the interface MAC address
-# is a known address. Instead, checks if the WiFi SSID
-# is a known SSID.
+# Basically it checks if an ethernet interface is connected then checks if the
+# interface MAC address is a known address. Instead, checks if the WiFi SSID is a
+# known SSID.
 #
-# Author: arendruni
-# Last Update: 26th October 2019
+# Author: dev@giun.io
+# Last Update: 26th March 2023
 
-# Get current location
+# get current location
 CURRENT_LOCATION=$(networksetup -getcurrentlocation)
 
-# Ethernet only Home location
-LOCATION_ETHERNET=$1
+# default network location
+DEFAULT_LOCATION=$1
 
-# Mac address of home ethernet interface
-ETH_MAC=$2
+# my location
+MY_LOCATION=$2
 
-# Home wifi location
-LOCATION_HOME=$3
+# mac address regex
+ETH_MAC=$3
 
-# Home SSID
+# SSIDs regex
 SSID=$4
 
-# Auto location
-LOCATION_AUTO=$5
-
-# Ethernet only location
-LOCATION_AUTO_ETHERNET=$6
-
-# Ethernet interface
-ETH_INT=$7
-
-# Display notification
-NOTIFICATION=$8
+# display notification when location changes
+NOTIFICATION=$5
 
 # check if ethernet cable is connected
-# check if the ethernet mac is kwown (at home)
-if (ifconfig | grep -q "$ETH_MAC"); then
-    # set new location to ethernet home
-    NEW_LOCATION=$LOCATION_ETHERNET
-    
-    elif ( ifconfig | grep -q "$ETH_INT" ); then
-    # instead, set ne location to generic ethernet
-    NEW_LOCATION=$LOCATION_AUTO_ETHERNET
-    
-    # look for known SSID
-    elif ( /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -q "$SSID" ); then
-    # set new location to home
-    NEW_LOCATION=$LOCATION_HOME
+if (ifconfig | grep -q -E "$ETH_MAC"); then
+    # set new location
+    NEW_LOCATION=$MY_LOCATION
+
+    echo "MAC address matched $ETH_MAC"
+# look for known SSIDs
+elif (/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo | grep -q -E "$SSID"); then
+    # set new location
+    NEW_LOCATION=$MY_LOCATION
+
+    echo "Active SSID matched $SSID"
 else
     # set new location to auto
-    NEW_LOCATION=$LOCATION_AUTO
+    NEW_LOCATION=$DEFAULT_LOCATION
+
+    echo "Nothing matched"
 fi
 
-# switch to new location only if it's changed from current location
+# switch to new location only if it changed from current location
 if [ "$CURRENT_LOCATION" != "$NEW_LOCATION" ]; then
     scselect "$NEW_LOCATION"
-    
+
+    echo "location changed from $CURRENT_LOCATION to $NEW_LOCATION"
+
     # display a notification
     if [ "$NOTIFICATION" == "--notification" ]; then
         osascript -e "display notification \"Network location switched from $CURRENT_LOCATION to $NEW_LOCATION\" with title \"Network Location Switcher\""
     fi
+else
+    echo "location not changed"
 fi
